8422486a18e908d17070e9dd05caa8bac1a1ab48
Søren Sjørup <zorren@gmail.com>
2017-04-29 14:12:23 +0200

    some smoke testing

--- BDDFS/BDDFS/BDDFS.fs
+++ BDDFS/BDDFS/BDDFS.fs
@@ -19,4 +19,9 @@ module BDDFS =
         let env = Map.ofList [1, true; 2, false; 3, true]
         printfn "%A" <| eval (fun v -> Map.find v env) bdd
         printfn "%A" <| evalExp (fun v -> Map.find v env) t
+        let t = b.Apply((fun(b1, b2) -> if b1 = 1 || b2 = 1 then 1 else 0), bddTopIndex, 1)
+        printfn "%A" <| t
+        let t2 = b.ApplyN((fun([|b1; b2|]) -> if b1 = 1 || b2 = 1 then 1 else 0), [|bddTopIndex; 1|])
+        printfn "%A" <| t2
+//        printfn "%A" << eval (fun v -> failwith "hej") <| b.GetExport t
         0 // return an integer exit code
